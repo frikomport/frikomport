2d922e099a381bb534c0ddfc2be34022a4c2f29d
Ida Guntveit <ida@knowit.no>
2008-06-04 08:53:22 +0000

    FKM-317 Ny UserDetailsController.java.
    git-svn-id: http://projects.knowit.no/repos/frikomport/trunk@173 b0a0c851-5f35-0410-b037-9bcbcc8e1326

---
+++ src/web/no/unified/soak/webapp/action/UserDetailsController.java
@@ -0,0 +1,40 @@
+/*
+ * This file is distributed under the GNU Public Licence (GPL).
+ * See http://www.gnu.org/copyleft/gpl.html for further details
+ * of the GPL.
+ *
+ * @author Unified Consulting AS
+*/
+package no.unified.soak.webapp.action;
+
+import javax.servlet.http.HttpServletRequest;
+
+import no.unified.soak.service.UserManager;
+
+
+/**
+ * Simple class to retrieve a list of users from the database.
+ *
+ * <p>
+ * <a href="UserController.java.html"><i>View Source</i></a>
+ * </p>
+ *
+ * @author <a href="mailto:matt@raibledesigns.com">Matt Raible</a>
+ */
+public class UserDetailsController extends BaseFormController {
+    private UserManager mgr = null;
+
+    public void setUserManager(UserManager userManager) {
+        this.mgr = userManager;
+    }
+
+    /* (non-Javadoc)
+     * @see org.springframework.web.servlet.mvc.AbstractFormController#formBackingObject(javax.servlet.http.HttpServletRequest)
+     */
+    protected Object formBackingObject(HttpServletRequest request)
+        throws Exception {
+    	String username = request.getParameter("username");
+        return mgr.getUser(username);
+    }
+    
+}
