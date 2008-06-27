0f539d6e63bae1d1cac992e65923d937dc5de3c2
Ida Guntveit <ida@knowit.no>
2008-06-27 08:50:26 +0000

    Oppdaterings script for databasen.
    git-svn-id: http://projects.knowit.no/repos/frikomport/trunk@246 b0a0c851-5f35-0410-b037-9bcbcc8e1326

---
+++ metadata/sql/update-to-1.5.sql
@@ -0,0 +1,7 @@
+﻿﻿ALTER TABLE organization ADD COLUMN address varchar(150);
+ALTER TABLE organization ADD COLUMN city varchar(50);
+ALTER TABLE organization ADD COLUMN province varchar(100);
+ALTER TABLE organization ADD COLUMN country varchar(100);
+ALTER TABLE organization ADD COLUMN postal_code varchar(15);
+ALTER TABLE organization ADD COLUMN invoicename varchar(100);
+
