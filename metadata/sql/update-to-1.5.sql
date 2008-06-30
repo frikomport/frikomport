ee4a44dc76c68d96ef3b7d807178bcce931f694d
Ida Guntveit <ida@knowit.no>
2008-06-30 12:56:39 +0000

    FKM-318 oppdatert sql script.
    git-svn-id: http://projects.knowit.no/repos/frikomport/trunk@251 b0a0c851-5f35-0410-b037-9bcbcc8e1326

--- metadata/sql/update-to-1.5.sql
+++ metadata/sql/update-to-1.5.sql
@@ -5,12 +5,14 @@ ALTER TABLE organization CHANGE COLUMN `country` `invoice_country` varchar(100);
 ALTER TABLE organization CHANGE COLUMN `postal_code` `invoice_postal_code` varchar(15);
 ALTER TABLE organization CHANGE COLUMN `invoicename` `invoice_name` varchar(100);
 
+ALTER TABLE registration ADD COLUMN invoice_address varchar(150);
 ALTER TABLE registration ADD COLUMN invoice_city varchar(50);
 ALTER TABLE registration ADD COLUMN invoice_province varchar(100);
 ALTER TABLE registration ADD COLUMN invoice_country varchar(100);
 ALTER TABLE registration ADD COLUMN invoice_postal_code varchar(15);
 ALTER TABLE registration ADD COLUMN invoice_name varchar(100);
 
+ALTER TABLE app_user ADD COLUMN invoice_address varchar(150);
 ALTER TABLE app_user ADD COLUMN invoice_city varchar(50);
 ALTER TABLE app_user ADD COLUMN invoice_province varchar(100);
 ALTER TABLE app_user ADD COLUMN invoice_country varchar(100);
