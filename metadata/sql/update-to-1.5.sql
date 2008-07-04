eb0bd512e71bd26aec246a96fa2913360007dead
Ida Guntveit <ida@knowit.no>
2008-07-04 09:04:46 +0000

    FKM-222 sql oppdateringen for prod databasen.
    git-svn-id: http://projects.knowit.no/repos/frikomport/trunk@259 b0a0c851-5f35-0410-b037-9bcbcc8e1326

--- metadata/sql/update-to-1.5.sql
+++ metadata/sql/update-to-1.5.sql
@@ -11,6 +11,7 @@ ALTER TABLE registration ADD COLUMN invoice_province varchar(100);
 ALTER TABLE registration ADD COLUMN invoice_country varchar(100);
 ALTER TABLE registration ADD COLUMN invoice_postal_code varchar(15);
 ALTER TABLE registration ADD COLUMN invoice_name varchar(100);
+ALTER TABLE registration ADD COLUMN closest_leader varchar(150);
 
 ALTER TABLE app_user ADD COLUMN invoice_address varchar(150);
 ALTER TABLE app_user ADD COLUMN invoice_city varchar(50);
@@ -18,4 +19,5 @@ ALTER TABLE app_user ADD COLUMN invoice_province varchar(100);
 ALTER TABLE app_user ADD COLUMN invoice_country varchar(100);
 ALTER TABLE app_user ADD COLUMN invoice_postal_code varchar(15);
 ALTER TABLE app_user ADD COLUMN invoice_name varchar(100);
+ALTER TABLE app_user ADD COLUMN closest_leader varchar(150);
 
