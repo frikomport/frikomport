5a9d0117a27411e1d79dd97a39c644ad594d8715
Gunnar Velle <gv@knowit.no>
2008-09-14 22:19:47 +0000

    git-svn-id: http://projects.knowit.no/repos/frikomport/tags@289 b0a0c851-5f35-0410-b037-9bcbcc8e1326

--- 1.5/metadata/sql/update-1.2-1.2.5.sql
+++
@@ -1,5 +0,0 @@
--- Script for upgrading the frikob db from version 1.2 to 1.2.5
- 
-ALTER TABLE COURSE ADD COLUMN ROLE VARCHAR(50) NOT NULL;
-ALTER TABLE COURSE ALTER COLUMN ROLE SET DEFAULT 'Anonymous';
-UPDATE COURSE SET ROLE = 'Anonymous';
