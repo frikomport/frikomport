737aeff3ad0291cd41b40940f30c6ddafe6efae8
Gunnar Velle <gv@knowit.no>
2008-10-22 08:15:08 +0000

    FKM-425 fikser mail til påmeldte
    git-svn-id: http://projects.knowit.no/repos/frikomport/trunk@303 b0a0c851-5f35-0410-b037-9bcbcc8e1326

--- src/web/no/unified/soak/webapp/action/CourseEmailController.java
+++ src/web/no/unified/soak/webapp/action/CourseEmailController.java
@@ -3,23 +3,44 @@ package no.unified.soak.webapp.action;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.mail.internet.MimeMessage;
 
 import no.unified.soak.Constants;
+import no.unified.soak.service.RegistrationManager;
+import no.unified.soak.util.MailUtil;
 import no.unified.soak.model.Course;
 import no.unified.soak.model.User;
+import no.unified.soak.model.Registration;
 
 import org.springframework.validation.BindException;
 import org.springframework.validation.Errors;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.context.MessageSource;
+import org.springframework.mail.MailSender;
 
 public class CourseEmailController extends CourseNotificationController
 {
+    private RegistrationManager registrationManager;
+    private MessageSource messageSource;
+    private MailSender mailSender;
 
-    protected Map referenceData(HttpServletRequest request, Object command, Errors errors) throws Exception {
+    public void setRegistrationManager(RegistrationManager registrationManager) {
+        this.registrationManager = registrationManager;
+    }
+    public void setMessageSource(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+    public void setMailSender(MailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public Map referenceData(HttpServletRequest request, Object command, Errors errors) throws Exception {
         if (log.isDebugEnabled()) {
             log.debug("entering 'referenceData' method...");
         }
@@ -63,11 +84,30 @@ public class CourseEmailController extends CourseNotificationController
             return new ModelAndView(getCancelView(), "id", course.getId().toString());
         } // or to send out notification email?
         else if (request.getParameter("send") != null) {
-            sendMail(locale, course, Constants.EMAIL_EVENT_NOTIFICATION, mailComment, mailSender, null);
+            sendMail(locale, course, Constants.EMAIL_EVENT_NOTIFICATION, mailComment, mailSender);
         }
 
         model.put("course",course);
 
         return new ModelAndView(getSuccessView(),model);
     }
+
+    	/**
+	 * Sends mail to the registered users
+	 *
+	 * @param locale
+	 *            The locale to use
+     * @param course
+     * @param from
+     */
+	private void sendMail(Locale locale, Course course, int event, String mailComment, String from) {
+		log.debug("Sending mail from CourseEmailController");
+		List<Registration> registrations = registrationManager.getSpecificRegistrations(course.getId(), null, null, true,null, null, null);
+        // Sender mail til kun reserverte.
+		StringBuffer msg= MailUtil.createStandardBody(course, event, locale, messageSource, mailComment,true);
+
+		ArrayList<MimeMessage> emails = MailUtil.getMailMessages(registrations, event, course, msg, messageSource, locale, from, mailSender);
+		MailUtil.sendMimeMails(emails, mailEngine);
+
+	}
 }
