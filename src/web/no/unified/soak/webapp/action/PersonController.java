bc31c19ddef0efc9c170a4ad623d06ec3278123c
Sindre Amundsen <sa@knowit.no>
2009-10-27 10:23:03 +0000

    FKM-399: Lagt til sjekk på brukerens rolle ved uthenting av personer (inaktive personer returneres ikke hvis anonym eller ansatt)
    git-svn-id: http://projects.knowit.no/repos/frikomport/trunk@373 b0a0c851-5f35-0410-b037-9bcbcc8e1326

--- src/web/no/unified/soak/webapp/action/PersonController.java
+++ src/web/no/unified/soak/webapp/action/PersonController.java
@@ -10,13 +10,16 @@
  */
 package no.unified.soak.webapp.action;
 
+import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import no.unified.soak.Constants;
 import no.unified.soak.model.Person;
+import no.unified.soak.model.User;
 import no.unified.soak.service.PersonManager;
 
 import org.apache.commons.beanutils.BeanUtils;
@@ -53,18 +56,34 @@ public class PersonController implements Controller {
         // populate object with request parameters
         BeanUtils.populate(person, request.getParameterMap());
 
-        List<Person> persons = getPersons();
+    	HttpSession session = request.getSession();
+    	User user = (User) session.getAttribute(Constants.USER_KEY);
+    	List roles = user.getRoleNameList();
+
+    	List<Person> persons = getPersons(roles);
 
         return new ModelAndView("personList", Constants.PERSON_LIST, persons);
     }
 
     /**
-     * Gets the list of persons
-     * @return A list of persons
+    * Gets the list of persons
+     * @param List of roles
+     * @return List of persons based on users role
      */
-    private List<Person> getPersons() {
-        // Get all persons
-        List<Person> persons = personManager.getPersons(null, new Boolean(true));
+    private List<Person> getPersons(List roles) {
+        // Anonymous / empoyees does not return disabled users 
+    	Boolean includeDisabledUsers = new Boolean(true);
+    	
+    	Iterator it = roles.listIterator();
+    	while(it.hasNext()) {
+    		String roleName = (String)it.next();
+    		if(Constants.ANONYMOUS_ROLE.equalsIgnoreCase(roleName) || Constants.EMPLOYEE_ROLE.equalsIgnoreCase(roleName)) {
+    			includeDisabledUsers = new Boolean(false);
+    			break;
+    		}
+    	}
+    	
+        List<Person> persons = personManager.getPersons(null, includeDisabledUsers);
         return persons;
     }
 }
