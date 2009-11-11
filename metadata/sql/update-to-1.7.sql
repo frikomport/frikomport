65a5e7278a9ba6e4b5d376c5269b4897a398c0d4
Sindre Amundsen <sa@knowit.no>
2009-11-11 10:03:36 +0000

    Endret freezeAttenance til å kunne være NULL i db/hibernate
    git-svn-id: http://projects.knowit.no/repos/frikomport/trunk@397 b0a0c851-5f35-0410-b037-9bcbcc8e1326

--- metadata/sql/update-to-1.7.sql
+++ metadata/sql/update-to-1.7.sql
@@ -1,4 +1,3 @@
 ALTER TABLE course ADD COLUMN chargeoverdue tinyint(1) default 0;
 
-ALTER TABLE course MODIFY COLUMN freezeAttendance not null;
-
+ALTER TABLE course MODIFY COLUMN freezeAttendance datetime;
