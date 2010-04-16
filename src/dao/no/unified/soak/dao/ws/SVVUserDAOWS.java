65dd4e04bc78aa332a7c2ade85f3b973857b6a4e
Klaus Stafto <kst@knowit.no>
2010-04-16 10:31:32 +0000

    * Webservice kall funker, men håndtering mangler.
    git-svn-id: http://projects.knowit.no/repos/frikomport/branches/SVV@449 b0a0c851-5f35-0410-b037-9bcbcc8e1326

--- src/dao/no/unified/soak/dao/ws/SVVUserDAOWS.java
+++ src/dao/no/unified/soak/dao/ws/SVVUserDAOWS.java
@@ -1,8 +1,16 @@
-/**
+ /**
  * 
  */
 package no.unified.soak.dao.ws;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -49,12 +57,6 @@ public class SVVUserDAOWS implements ExtUserDAO {
         this.endpoint = endpoint;
     }
 
-    private FkpUserPortType getPort() throws ServiceException {
-        FkpUser_ServiceLocator locator = new FkpUser_ServiceLocator();
-        locator.setEndpointAddress("LDAPUserPort", endpoint);
-        FkpUserPortType port = locator.getFkpUserPort();
-        return port;
-    }
 
     public void setRoleDAO(RoleDAO roleDAO) {
         this.roleDAO = roleDAO;
@@ -110,19 +112,7 @@ public class SVVUserDAOWS implements ExtUserDAO {
     public ExtUser findUserByUsername(String username) {
         // TODO Klaus: SVV-tjeneste for henting av brukerinfo mangler, dummy foreløpig.
         try {
-            // FkpUser_Type user = getPort().getUser(username);
-
-            String requestXML = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">" 
-//                xmlns:urn=\"urn:no:vegvesen:ldap.wsdl.OppslagSVVAnsattContract:cc:1:0\" xmlns:urn1=\"urn:no:vegvesen:ldap:soa2:SVVUIDType:cct:1:0\">"
-                    + "<soapenv:Body><urn:Request><urn1:UID>"
-                    + username
-                    + "</urn1:UID></urn:Request></soapenv:Body></soapenv:Envelope>";
-            Service service = new Service();
-            Call call = (Call) service.createCall();
-            call.setTargetEndpointAddress(new java.net.URL(endpoint));
-            call.setOperationName(new QName("opOppslagSVVAnsatt"));
-//            String soapResponse = (String) call.invoke(new Object[] { requestXML });
-//            log.debug("Got: [" + soapResponse + "]");
+            getExtUserFromWebservice(username);
         } catch (Exception e) {
             log.error("Feilet ved henting av user fra webservice", e);
         }
@@ -134,6 +124,70 @@ public class SVVUserDAOWS implements ExtUserDAO {
        
         return extUser;
     }
+
+	public ExtUser getExtUserFromWebservice(String username) {
+		ExtUser extuser = null;
+		try {
+			URL url = new URL("http://svvjcapsu04.vegvesen.no:18201/ldap_searchemployees/portOppslagSVVAnsattBndPort");
+			URLConnection connection = url.openConnection();
+			connection.setDoOutput(true);
+			connection.setDoInput(true); //Only if you expect to read a response...
+			connection.setUseCaches(false); //Highly recommended...
+			connection.setRequestProperty("Content-Type", "text/xml;charset=UTF-8");
+			connection.setRequestProperty("SOAPAction", 
+					"\"urn:no:vegvesen:ldap.wsdl:OppslagSVVAnsatt:1:0/portOppslagSVVAnsatt/opOppslagSVVAnsatt\"");
+			connection.setRequestProperty("Host", "svvjcapsu04.vegvesen.no:18201");
+
+            String requestXML = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:urn=\"urn:no:vegvesen:ldap.wsdl.OppslagSVVAnsattContract:cc:1:0\" xmlns:urn1=\"urn:no:vegvesen:ldap:soa2:SVVUIDType:cct:1:0\">"
+                  + "<soapenv:Body><urn:Request><urn1:UID>"
+                  + username
+                  + "</urn1:UID></urn:Request></soapenv:Body></soapenv:Envelope>";
+            
+            connection.setRequestProperty("Content-Length", String.valueOf(requestXML.length()));
+            PrintWriter output = new PrintWriter(new OutputStreamWriter(connection.getOutputStream()));
+            output.print(requestXML);
+            output.flush();
+            output.close();
+
+            InputStream inputStream = connection.getInputStream();
+            String responseString = convertStreamToString(inputStream);
+            System.out.println(responseString);
+            extuser = new ExtUser();
+            
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return extuser ;
+	}
+
+	public static String convertStreamToString(InputStream is) throws IOException {
+		/*
+		 * To convert the InputStream to String we use the
+		 * BufferedReader.readLine() method. We iterate until the BufferedReader
+		 * return null which means there's no more data to read. Each line will
+		 * appended to a StringBuilder and returned as String.
+		 */
+		if (is != null) {
+			StringBuilder sb = new StringBuilder();
+			String line;
+
+			try {
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(is, "UTF-8"));
+				while ((line = reader.readLine()) != null) {
+					sb.append(line).append("\n");
+				}
+			} finally {
+				is.close();
+			}
+			return sb.toString();
+		} else {
+			return "";
+		}
+	}
 
     private ExtUser getHardcodedExtUser(String username) {
         if (hardcodedExtUsers.isEmpty()) {
