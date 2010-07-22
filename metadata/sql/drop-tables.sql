3b1c9ab80365543caa815926824b54cdb35bfcc9
Klaus Stafto <kst@knowit.no>
2010-07-22 15:08:48 +0000

    Script for å tømme databasen så den kan opprettes på nytt ved neste applikasjonsstart.
    git-svn-id: http://projects.knowit.no/repos/frikomport/branches/SVV@670 b0a0c851-5f35-0410-b037-9bcbcc8e1326

---
+++ metadata/sql/drop-tables.sql
@@ -0,0 +1,14 @@
+drop table configuration;
+drop table user_role;
+drop table role;
+drop table notification;
+drop table attachment;
+drop table registration;
+drop table course;
+drop table person;
+drop table location;
+drop table app_user;
+drop table servicearea;
+drop table organization;
+drop table category;
+drop table user_cookie;
